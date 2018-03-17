969bdceb85f018b8e2519fc99d350a6f21f426b6
Mikey <Mikey@DESKTOP-0Q2I46R>
2018-03-17 11:24:50 +0200

    Change name of function and add second sub task in first task

--- Kurshakov_3/Kurshakov_3/Kurshakov_3/Program.cs
+++ Kurshakov_3/Kurshakov_3/Kurshakov_3/Program.cs
@@ -22,13 +22,17 @@ namespace Kurshakov_3
             Random rand = new Random();
             for (i = 0; i < elements_amount; i++)
                 array_sort[i]=rand.Next(0, 50);
-            Show_array(array_sort);
+            ShowArray(array_sort);
 
-            Console.WriteLine("From min to max");
-            Sort_array(array_sort, Sort.bottom_up);
+            Console.WriteLine("\nFrom min to max");
+            SortArray(array_sort, Sort.bottom_up);
+            IsSort(array_sort, Sort.bottom_up);
+            IsSort(array_sort, Sort.up_bottom);
 
-            Console.WriteLine("From max to min");
-            Sort_array(array_sort, Sort.up_bottom);
+            Console.WriteLine("\nFrom max to min");
+            SortArray(array_sort, Sort.up_bottom);
+            IsSort(array_sort, Sort.up_bottom);
+            IsSort(array_sort, Sort.bottom_up);
 
 
             //TASK 2
@@ -39,7 +43,7 @@ namespace Kurshakov_3
             double start_element = 2;  //    a1
             double result = 1;
 
-            result = start_element * Recursion_count(start_element + progression_step, progression_step, elements_amount);
+            result = start_element * RecursionCount(start_element + progression_step, progression_step, elements_amount);
             Console.WriteLine("Написать функцию, возвращающую " +
                 "произведение n \nэлементов арифметической прогрессии " +
                 "чисел с первым элементом a1 и шагом t (an = an-1 + t)." +
@@ -54,7 +58,7 @@ namespace Kurshakov_3
             double element_lim = 0.1;   //    alim
 
             result = 1;
-            result = start_element * Recursion_for_down_count(start_element/progression_step, progression_step, element_lim);
+            result = start_element * RecursionForDownCount(start_element/progression_step, progression_step, element_lim);
             Console.WriteLine("Написать функцию, возвращающую " +
     "произведение n \nэлементов геометрической прогрессии " +
     "чисел с первым элементом a1 и шагом t (an = an-1//t)." +
@@ -64,26 +68,59 @@ namespace Kurshakov_3
     , element_lim, result);
 
         }
-        static void Show_array(int[] array_show)
+        static void ShowArray(int[] array_show)
         {
             int i;
             for (i = 0; i < array_show.Length; i++)
                 Console.Write(" {0} ", array_show[i]);
             Console.Write("\n");
         }
-        static void Sort_array(int[] array, Sort sort_criterion)
+        static void SortArray(int[] array, Sort sort_criterion)
         {
             switch(sort_criterion)
             {
                 case Sort.bottom_up:
-                    Sort_from_bottom(array);
+                    SortFromBottom(array);
                     break;
                 case Sort.up_bottom:
-                    Sort_from_up(array);
+                    SortFromUp(array);
                     break;
             }
         }
-        static void Sort_from_bottom(int[] array)
+        static void IsSort(int[] array, Sort sort_criterion)
+        {
+            int i;
+            bool flag=true;
+            switch (sort_criterion)
+            {
+                case Sort.bottom_up:
+                    for (i = 0; i < array.Length - 1; i++)
+                        if (array[i + 1] < array[i])
+                        {
+                            flag = false;
+                          break;
+                        }
+                    if (flag)
+                        Console.WriteLine("Массив отсортирован по возростанию");
+                    else
+                        Console.WriteLine("Ошибка, массив не отсортирован по возростанию");
+                    break;
+                case Sort.up_bottom:
+                    flag = true;
+                    for (i = 0; i < array.Length - 1; i++)
+                        if (array[i + 1] > array[i])
+                        {
+                            flag = false;
+                            break;
+                        }
+                    if (flag)
+                        Console.WriteLine("Массив отсортирован в порядке убывания");
+                    else
+                        Console.WriteLine("Ошибка, массив не отсортирован в порядке убывания");
+                    break;
+            }
+        }
+        static void SortFromBottom(int[] array)
         {
             int i,j;
             int buffer;
@@ -97,9 +134,9 @@ namespace Kurshakov_3
                         array[j + 1] = buffer;
                     }
     
-            Show_array(array);     
+            ShowArray(array);     
         }
-        static void Sort_from_up(int[] array)
+        static void SortFromUp(int[] array)
         {
             int i, j;
             int buffer;
@@ -112,25 +149,25 @@ namespace Kurshakov_3
                         array[j] = array[j + 1];
                         array[j + 1] = buffer;
                     }
-            Show_array(array);
+            ShowArray(array);
         }
-        static double Recursion_count(double current_element,double step,int counter)
+        static double RecursionCount(double current_element,double step,int counter)
         {
             double result = 1;
             if (counter > 1)
             {
-                result=current_element*Recursion_count(current_element + step, step, counter - 1);
+                result=current_element*RecursionCount(current_element + step, step, counter - 1);
             }
             else
                 return 1;
             return result;
         }
-        static double Recursion_for_down_count(double current_element, double step, double boarder)
+        static double RecursionForDownCount(double current_element, double step, double boarder)
         {
             double result = 1;
             if (current_element > boarder)
             {
-                result = current_element * Recursion_for_down_count(current_element/step, step, boarder);
+                result = current_element * RecursionForDownCount(current_element/step, step, boarder);
             }
             else
                 return 1;
