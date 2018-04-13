5ea8aa398b17cd9fd587374b85f55abe9a757403
MikeKurshakov <kurshakov.mihail@gmail.com>
2018-04-13 19:34:59 +0300

    5 HomeWork

---
+++ Kurshakov_5/ArrayClass/MyArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace ArrayClass
+{
+    public class MyArray
+    {
+        private int[] builtArray;
+
+        public int LowLim { get; private set; }
+        public int UppLim { get; private set; }
+        public int GetLength { get; private set; }
+
+        public MyArray(int low, int high)
+        {
+            builtArray = new int[high - low+1];
+
+            GetLength = high - low+1;
+            LowLim = low;
+            UppLim = high;
+        }
+
+
+
+
+        public int this[int index]
+        {
+            get { return builtArray[index - UppLim]; }
+            set
+            {
+               builtArray[index - LowLim] = value;
+            }
+        }
+    }
+}
---
+++ Kurshakov_5/Kurshakov_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using ArrayClass;
+
+
+namespace Kurshakov_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowLim = 10,uppLim=20,i;
+            MyArray myArr = new MyArray(lowLim, uppLim);
+            int length,low,upp;
+
+            for (i = lowLim; i < uppLim+1; i++)
+            {
+                myArr[i] = i;
+            }
+
+            low = myArr.LowLim;
+            upp = myArr.UppLim;
+            length = myArr.GetLength;
+
+        }
+    }
+}
