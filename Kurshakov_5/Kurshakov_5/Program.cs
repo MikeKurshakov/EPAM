15232f7aad0d3708bf06ab05bd1beb91666f5907
MikeKurshakov <kurshakov.mihail@gmail.com>
2018-04-16 13:23:36 +0300

    Add task for polynom

--- Kurshakov_5/Kurshakov_5/Program.cs
+++ Kurshakov_5/Kurshakov_5/Program.cs
@@ -2,6 +2,7 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using ArrayClass;
+using Polynom;
 
 
 namespace Kurshakov_5
@@ -10,6 +11,7 @@ namespace Kurshakov_5
     {
         static void Main(string[] args)
         {
+            //TASK 1
             int lowLim = 10,uppLim=20,i;
             MyArray myArr = new MyArray(lowLim, uppLim);
             int length,low,upp;
@@ -22,6 +24,27 @@ namespace Kurshakov_5
             low = myArr.LowLim;
             upp = myArr.UppLim;
             length = myArr.GetLength;
+
+            //TASK 2
+            int[] coeffArray1 = new int[] { 1, -3, -5, 7, 9 };
+            int[] coeffArray2 = new int[] { 7, 2, 4, 3 };
+            int power1,power2;
+            string showPolynom1,showPolynom2;
+
+            MyPolynom polynom1=new MyPolynom(coeffArray1);
+            MyPolynom polynom2 = new MyPolynom(coeffArray2);
+
+            power1 = polynom1.PolynomPower;
+            power2 = polynom2.PolynomPower;
+
+            showPolynom1 = polynom1.ShowPolynom;
+            showPolynom2 = polynom2.ShowPolynom;
+
+            MyPolynom polynomAdd = polynom1 + polynom2;
+            MyPolynom polynomSub = polynom1 - polynom2;
+            MyPolynom polynomMult = polynom1 * polynom2;
+
+
 
         }
     }
---
+++ Kurshakov_5/Polynom/MyPolynom.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Polynom
+{
+    public class MyPolynom
+    {
+        private int[] coeffArray;
+
+        public int PolynomPower { get; private set; }
+
+        public string ShowPolynom
+        {
+            get
+            {
+                string result = "Polynom: ", element = null;
+                for (int i = coeffArray.Length - 1; i >= 0; i--)
+                {
+                    if ((coeffArray[i] >= 0) && (i != 0) && (i != coeffArray.Length - 1))
+                    {
+                        element = string.Concat("+", coeffArray[i].ToString(), "x^", i);
+                    }
+                    else if ((coeffArray[i] < 0) && (i != 0) && (i != coeffArray.Length - 1))
+                    {
+                        element = string.Concat("", coeffArray[i].ToString(), "x^", i);
+                    }
+                    else if ((i == 0) && (coeffArray[i] >= 0))
+                    {
+                        element = string.Concat("+", coeffArray[i].ToString());
+                    }
+                    else if ((i == 0) && (coeffArray[i] < 0))
+                    {
+                        element = string.Concat("", coeffArray[i].ToString());
+                    }
+                    else if (i == coeffArray.Length - 1)
+                    {
+                        element = string.Concat("", coeffArray[i].ToString(), "x^", i);
+                    }
+
+                    result = string.Concat(result, element);
+                }
+
+                return result;
+            }
+        }
+
+
+        public MyPolynom(int[] coeffArray)
+        {
+            this.coeffArray = coeffArray;
+            PolynomPower = coeffArray.Length - 1;
+        }
+
+        public int this[int index]
+        {
+            get { return coeffArray[index]; }
+            set { coeffArray[index] = value; }
+        }
+
+        public static MyPolynom operator +(MyPolynom lPolynom, MyPolynom rPolynom)
+        {
+            int[] newCoeff = new int[Math.Max(lPolynom.PolynomPower + 1, rPolynom.PolynomPower + 1)];
+            for (int i = 0; i < Math.Min(lPolynom.PolynomPower + 1, rPolynom.PolynomPower + 1); i++)
+            {
+                newCoeff[i] = lPolynom[i] + rPolynom[i];
+            }
+
+            if (Math.Max(lPolynom.PolynomPower + 1, rPolynom.PolynomPower + 1) == lPolynom.PolynomPower + 1)
+                for (int i = rPolynom.PolynomPower + 1; i < lPolynom.PolynomPower + 1; i++)
+                {
+                    newCoeff[i] = lPolynom[i];
+                }
+            else
+                for (int i = lPolynom.PolynomPower + 1; i < rPolynom.PolynomPower + 1; i++)
+                {
+                    newCoeff[i] = rPolynom[i];
+                }
+
+            return new MyPolynom(newCoeff);
+        }
+
+        public static MyPolynom operator -(MyPolynom lPolynom, MyPolynom rPolynom)
+        {
+            int[] newCoeff = new int[Math.Max(lPolynom.PolynomPower + 1, rPolynom.PolynomPower + 1)];
+            for (int i = 0; i < Math.Min(lPolynom.PolynomPower + 1, rPolynom.PolynomPower + 1); i++)
+            {
+                newCoeff[i] = lPolynom[i] - rPolynom[i];
+            }
+
+            if (Math.Max(lPolynom.PolynomPower + 1, rPolynom.PolynomPower + 1) == lPolynom.PolynomPower + 1)
+                for (int i = rPolynom.PolynomPower + 1; i < lPolynom.PolynomPower + 1; i++)
+                {
+                    newCoeff[i] = lPolynom[i];
+                }
+            else
+                for (int i = lPolynom.PolynomPower + 1; i < rPolynom.PolynomPower + 1; i++)
+                {
+                    newCoeff[i] = -rPolynom[i];
+                }
+
+            return new MyPolynom(newCoeff);
+        }
+
+        public static MyPolynom operator *(MyPolynom lPolynom, MyPolynom rPolynom)
+        {
+            int[] newCoeff = new int[lPolynom.PolynomPower + rPolynom.PolynomPower + 1];
+            for (int i = 0; i < lPolynom.PolynomPower + 1; ++i)
+            for (int j = 0; j < rPolynom.PolynomPower + 1; ++j)
+                newCoeff[i + j] += lPolynom[i] * rPolynom[j];
+            return new MyPolynom(newCoeff);
+
+        }
+    }
+}
