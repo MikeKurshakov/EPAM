d19453adec8b9f8fa134aea9fadd0d0ee862fb92
MikeKurshakov <kurshakov.mihail@gmail.com>
2018-04-16 18:59:03 +0300

    try{}catch{} and matrix override operations

---
+++ Kurshakov_6/Kurshakov_6/Matrix.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurshakov_6
+{
+    class Matrix
+    {
+        private int[,] coeffArray;
+
+        public Matrix(int[,] coeffArray)
+        {
+            this.coeffArray = coeffArray;
+        }
+        public int this[int index1, int index2]
+        {
+            get { return coeffArray[index1,index2]; }
+            set { coeffArray[index1,index2] = value; }
+        }
+
+        public int[] GetDimentional()
+        {
+            return new int[] {coeffArray.GetLength(0), coeffArray.GetLength(1)};
+        }
+        public static Matrix operator +(Matrix lMatrix,Matrix rMatrix)
+        {
+            int[,] newCoeff = new int[lMatrix.coeffArray.GetLength(0), lMatrix.coeffArray.GetLength(1)];
+            for (int i = 0; i < lMatrix.coeffArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < lMatrix.coeffArray.GetLength(1); j++)
+                {
+                    newCoeff[i, j] = lMatrix[i, j] + rMatrix[i, j];
+                }
+                
+            }
+            return new Matrix(newCoeff);
+        }
+        public static Matrix operator -(Matrix lMatrix, Matrix rMatrix)
+        {
+            int[,] newCoeff = new int[lMatrix.coeffArray.GetLength(0), lMatrix.coeffArray.GetLength(1)];
+            for (int i = 0; i < lMatrix.coeffArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < lMatrix.coeffArray.GetLength(1); j++)
+                {
+                    newCoeff[i, j] = lMatrix[i, j] - rMatrix[i, j];
+                }
+
+            }
+            return new Matrix(newCoeff);
+        }
+        public static Matrix operator *(Matrix lMatrix, Matrix rMatrix)
+        {
+            int[,] newCoeff = new int[lMatrix.coeffArray.GetLength(0), rMatrix.coeffArray.GetLength(1)];
+            int summ = 0;
+            for (int i = 0; i < newCoeff.GetLength(0); i++)
+            {
+                for (int j = 0; j < newCoeff.GetLength(1); j++)
+                {
+                    for (int k = 0; k < Math.Max(rMatrix.coeffArray.GetLength(0),rMatrix.coeffArray.GetLength(1)); k++)
+                    {
+                        newCoeff[i, j] += lMatrix[i, k] * rMatrix[k, j];
+                    }
+                }
+            }
+            return new Matrix(newCoeff);
+        }
+    }
+}
---
+++ Kurshakov_6/Kurshakov_6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurshakov_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] coeffArray1 = new int[,] { { 1, 2,3 }, { 4,5,6 }};
+            int [,] coeffArray2 = new int[,] { { 9, 1 } , { 2, 3 }, {4, 5} };
+            Matrix matrix1= new Matrix(coeffArray1);
+            Matrix matrix2 = new Matrix(coeffArray2);
+
+            try
+            {
+                Matrix matrixAdd = matrix1 + matrix2;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Размерность матриц должна совпадать.");
+            }
+            try
+            {
+
+                Matrix matrixSub = matrix1 - matrix2;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Размерность матриц должна совпадать.");
+            }
+            try
+            {
+                if (matrix1.GetDimentional()[1] != matrix2.GetDimentional()[0])
+                {
+                    throw new IndexOutOfRangeException("Размерность матриц должна согласовываться [n;m] и [m;k].");
+                }
+                Matrix matrixMult = matrix1 * matrix2;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
